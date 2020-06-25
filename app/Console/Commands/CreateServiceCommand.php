3e4c2c16b9a5c0e9482b90d99e35c90e0da194b2
Eric <eric.mdoj8@gmail.com>
2020-06-24 22:13:10 -0500

    Add Artisan's command description

--- app/Console/Commands/CreateServiceCommand.php
+++ app/Console/Commands/CreateServiceCommand.php
@@ -7,25 +7,32 @@ use Illuminate\Console\GeneratorCommand;
 
 class CreateServiceCommand extends GeneratorCommand
 {
-    /**
+        /**
      * The name of your command. 
-     * This replaces $signature.
+     * This is how your Artisan's command shall be invoked.
      */
     protected $name = 'service:create'; 
 
     /**
-     * Description of Commands purpose.
+     * A short description of the command's purpose.
+     * You can see this working by executing
+     * php artisan list
      */
     protected $description = 'Create a Service Class file';
 
     
     /**
      * Class type that is being created.
+     * If command is executed successfully you'll receive a
+     * message like this: $type created succesfully.
+     * If the file you are trying to create already 
+     * exists, you'll receive a message
+     * like this: $type already exists!
      */
     protected $type = 'Service'; 
 
     /**
-     * Stub's location.
+     * Specify your Stub's location.
      */
     protected function getStub()
     {
@@ -33,7 +40,8 @@ class CreateServiceCommand extends GeneratorCommand
     }
 
     /**
-     * The root location where the file should be written to.
+     * The root location where your new file should 
+     * be written to.
      */
     protected function getDefaultNamespace($rootNamespace)
     {
